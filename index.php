4f9f897277f75d658fd2f918329540b64bed7711
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2023-10-19 08:43:46 -0300

    Criação do arquivo da página inicial

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GetTron - A Tecnologia ao Seu Alcance</title>
+</head>
+
+<body>
+    <header>
+        <nav></nav>
+    </header>
+
+    <main>
+        <h1>Olá, Mundo!</h1>
+    </main>
+
+    <footer></footer>
+</body>
+
+</html>
