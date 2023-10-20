a244b0ddabd0f5dc7efcc24f925873ddaa33175b
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2023-10-20 18:31:37 -0300

    Configuração inicial

--- index.php
+++ index.php
@@ -2,21 +2,28 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GetTron - A Tecnologia ao Seu Alcance</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>GetTron - A Tecnologia ao Seu Alcance</title>
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="assets/logo/gettron-logo.png" type="image/x-icon" />
+  <!-- CSS -->
+  <link rel="stylesheet" href="assets/styles/style.css" />
+  <link rel="stylesheet" href="assets/styles/style-home.css" />
 </head>
 
 <body>
-    <header>
-        <nav></nav>
-    </header>
+  <header>
+    <nav></nav>
+  </header>
 
-    <main>
-        <h1>Olá, Mundo!</h1>
-    </main>
+  <main>
+    <!-- Capa do site -->
+    <section id="capa"></section>
+  </main>
 
-    <footer></footer>
+  <footer></footer>
 </body>
 
 </html>
