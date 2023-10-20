d12de51c229f5cbd99a007d1b11098acc1109707
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2023-10-20 19:23:53 -0300

    Logo do header

--- index.php
+++ index.php
@@ -2,28 +2,37 @@
 <html lang="pt-br">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>GetTron - A Tecnologia ao Seu Alcance</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>GetTron - A Tecnologia ao Seu Alcance</title>
 
-  <!-- Favicon -->
-  <link rel="shortcut icon" href="assets/logo/gettron-logo.png" type="image/x-icon" />
-  <!-- CSS -->
-  <link rel="stylesheet" href="assets/styles/style.css" />
-  <link rel="stylesheet" href="assets/styles/style-home.css" />
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/logo/gettron-logo.png" type="image/x-icon" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/styles/style.css" />
+    <link rel="stylesheet" href="assets/styles/style-home.css" />
 </head>
 
 <body>
-  <header>
-    <nav></nav>
-  </header>
+    <header>
+        <div>
+            <div id="logo-header">
+                <div class="logo-imagem"><img src="assets/logo/gettron-logo.png" alt="Logo do GetTron"></div>
+                <div class="logo-texto">
+                    <h1>Get Tron</h1>
+                    <p>A Tecnologia ao Seu Alcance</p>
+                </div>
+            </div>
+            <nav></nav>
+        </div>
+    </header>
 
-  <main>
-    <!-- Capa do site -->
-    <section id="capa"></section>
-  </main>
+    <main>
+        <!-- Capa do site -->
+        <section id="capa"></section>
+    </main>
 
-  <footer></footer>
+    <footer></footer>
 </body>
 
 </html>
